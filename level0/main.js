8e92efac50c3824c850e8bde6a777e3d16da4140
rahulpatle-sol <patler044@gmail.com>
2025-12-28 04:14:56 +0530

    varibale done

---
+++ level0/main.js
@@ -0,0 +1,48 @@
+//  
+// 
+// topic 
+// 1 varibale 
+//  in javascript we have 3 types of variables
+// 1. var
+// 2. let
+// 3. const
+// 
+// var is a type of variable in javascript that can be reassigned and redeclared.
+// var is a global variable that can be accessed from anywhere in the code.
+// var is a function-scoped variable that can be accessed within the function it is declared in.
+// var is a block-scoped variable that can be accessed within the block it is declared in.
+// var is a type of variable in javascript that can be reassigned and redeclared.
+
+
+
+//  let is a type of variable in javascript that can be reassigned but not redeclared.
+//  let is a global variable that can be accessed from anywhere in the code.
+//  let is a function-scoped variable that can be accessed within the function it is declared in.
+//  let is a block-scoped variable that can be accessed within the block it is declared in.
+//  let is a type of variable in javascript that can be reassigned but not redeclared.
+
+
+
+//  const is a type of variable in javascript that cannot be reassigned or redeclared.
+//  const is a global variable that can be accessed from anywhere in the code.
+//  const is a function-scoped variable that can be accessed within the function it is declared in.
+//  const is a block-scoped variable that can be accessed within the block it is declared in.
+//  const is a type of variable in javascript that cannot be reassigned or redeclared.
+
+
+
+//  l
+let a = 10;
+console.log(a);
+const time = 12;
+console.log(time);
+var b = 20;
+console.log(b);
+let c = 30;
+console.log(c);  
+
+//  more about let and const 
+// 
+// 
+// let is a scope based variable 
+// 
