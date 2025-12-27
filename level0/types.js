b5a705d45555e2b40d6822d0cd659f345a85cf3b
rahulpatle-sol <patler044@gmail.com>
2025-12-28 04:27:40 +0530

    types adde

---
+++ level0/types.js
@@ -0,0 +1,20 @@
+//  the ultimate datata types in  js s
+// 
+//  javascript is a dynamic types language 
+// all  the data types in javascript are dynamic and can be changed at runtime. 
+// 
+// 
+// let is a scope based variable 
+// 
+//  primitive data types in javascript are: 
+//  number, string, boolean, null, undefined, symbol, bigint
+// 
+// 
+let num = 42;
+let str = "hello";
+let bool = true;
+let n = null;
+let u = undefined;
+let sym = Symbol("symbol");
+let bigInt = BigInt(123456789012345678901234567890);//  can be used to represent large integers that exceed the safe integer limit of Number.MAX_SAFE_INTEGER.
+console.log(num, str, bool, n, u, sym, bigInt)
